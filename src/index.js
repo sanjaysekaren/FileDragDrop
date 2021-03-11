59eecc074ea42a03dec6313d56f936c54105e027
lineldcosta <lineldcosta@gmail.com>
2021-03-11 12:39:02 +0530

    Config file changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import FileDragDropCompoenent from "./component/FileDragDrop";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import FileDragDropCompoenent from './component/FileDragDrop'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,13 +10,13 @@ ReactDOM.render(
       customOuterDropAreaStyle={{
         height: 150,
         width: 500,
-        border: "dashed gray 5px ",
-        borderRadius: "6px",
+        border: 'dashed gray 5px ',
+        borderRadius: '6px',
       }}
-      dropText={"drop here"}
+      dropText={'drop here'}
       isDropTextVisible
       multiple
-      acceptableExtensions={["pdf", "jpeg", "jpg"]}
+      acceptableExtensions={['pdf', 'jpeg', 'jpg']}
       acceptableSize={5}
       // fileNameRegex={/^([a-z0-9])$/}
       fileCountLimit={3}
@@ -26,10 +25,5 @@ ReactDOM.render(
       Drag and Drop file here
     </FileDragDropCompoenent>
   </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById('root'),
+)
